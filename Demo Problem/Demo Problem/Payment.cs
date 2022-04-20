73fa4d492d4e1fecd97e58ce61ee56ef27b3066b
PSUMIT1995 <psumit191.sp@gmail.com>
2022-04-20 10:13:07 +0530

    UC1 : Demo :

---
+++ Demo Problem/Demo Problem/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Problem
+{
+    internal class Payment
+    {
+        public void monthlyPayment(double p, double y, double r)
+        {
+            double rateOfIntrest = r / (12 * 100);
+            double n = 12 * y;
+            double payment = (p * rateOfIntrest) / (1 - Math.Pow((1 + rateOfIntrest), (-n)));
+            Console.WriteLine( "Monthly Payment Is " +payment);
+            
+        }
+    }
+}
--- Demo Problem/Demo Problem/Program.cs
+++ Demo Problem/Demo Problem/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Demo_Problem;
+using System;
+
+
+Payment myobj = new Payment();
+myobj.monthlyPayment(75000, 2012, 12560);
